04d129258516cd5b79787b1ac15b248bb1ff1bce
chocochuyd1 <48980359+chocochuyd1@users.noreply.github.com>
2019-12-03 18:41:04 -0600

    Add files via upload

--- Agenda/PlaceHolderContact.java
+++ Agenda/PlaceHolderContact.java
@@ -6,36 +6,57 @@ import com.softtek.java.academy.service.contactServiceData;
 import com.softtek.java.academy.service.contactServiceDataMemoryImp;
 
 public class PlaceHolderContact {
-	
+
 	private Contact contact;
 	private Contact friend;
-	
+	private Contact friend2;
+
 	public static void main(String[] args) {
 		PlaceHolderContact p = new PlaceHolderContact();
 		p.crearContactos();
-		
+
 		contactServiceData csd = new contactServiceDataMemoryImp();
 		Contact myContact = csd.createContact(p.contact);
 		Contact myFriendContact = csd.createContact(p.friend);
-		
-		Contact[] contacts = csd.findAll();
-		
-		for (Contact c : contacts) {
-			System.out.println(c);
+		Contact myOtherFriendContact = csd.createContact(p.friend2);
+		Contact[] contacts = csd.findByContactType(contactType.FRIEND);
+		if (contacts != null) {
+			for (Contact c : contacts) {
+				System.out.println(c);
+			}
+		} else {
+			System.out.println("No se encontraron resultados del tipo " + contactType.FRIEND);
 		}
-		
+		Contact[] contactsWork = csd.findByContactType(contactType.WORK);
+		if (contactsWork != null) {
+			for (Contact c : contactsWork) {
+				System.out.println(c);
+			}
+		} else {
+			System.out.println("No se encontraron resultados del tipo " + contactType.WORK);
+		}
 	}
-	
-    public void crearContactos() {
-        contact = new Contact();
-        contact.setFirstName("John");
-        contact.setLastName("Doe");
-        contact.setMotherName("Chaser");
-        contact.setEmail("java-academy@softtek.com");
-        contact.setPhoneNumber("0123456789");
 
-        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.FRIEND );
+	public void crearContactos() {
+		contact = new Contact();
+		contact.setFirstName("John");
+		contact.setLastName("Doe");
+		contact.setMotherName("Chaser");
+		contact.setEmail("java-academy@softtek.com");
+		contact.setPhoneNumber("0123456789");
 
-    }
+		friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102",
+				contactType.FRIEND);
+		friend2 = new Contact("2", "Jess", "Alvar", "Aia", "yuid1@hotmail.com", "8114565102", contactType.WORK);
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.FAMILIAR );
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.FAMILIAR );
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.WORK );
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.WORK );
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.WORK );
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.FAMILIAR );
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.WORK );
+//        friend = new Contact("1", "Jesus", "Alvarado", "Alicia", "chuyd1@hotmail.com", "8116535102", contactType.FRIEND );
+
+	}
 
 }
